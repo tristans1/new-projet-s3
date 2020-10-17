9e4ddac951a8c4efc38b9e9040ca81c8aeea921b
Tristan Seclet <tristanseclet@gmail.com>
2020-10-17 11:37:50 +0200

    fix : body scroll hidden in homepage only

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,24 +1,30 @@
 <template>
   <div class="map">
-    <TheNavigation />
-    
-    <GmapMap
-  v-bind:center="{lat:47.837299, lng:2.353695}"
-  :zoom="7"
-  map-type-id="terrain"
-  style="width: 100%; height: 90vh;"
->
-  <GmapMarker
-   v-bind:key="index"
-    v-for="(m, index) in markers"
-    v-bind:position="m.position"
-    v-bind:clickable="true"
-    :draggable="true"
-    @click="center=m.position"
-  />
-</GmapMap>
+    <TheNavigation/>
 
-<Messagerie />
+    <GmapMap
+        v-bind:center="{lat:47.837299, lng:2.353695}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 100%; height: 90vh;"
+        ref="mapRef"
+        :options="{
+          zoomControl: false,
+          streetViewControl: false,
+          scaleControl: false,
+           }"
+    >
+      <GmapMarker
+          v-bind:key="index"
+          v-for="(m, index) in markers"
+          v-bind:position="m.position"
+          v-bind:clickable="true"
+          :draggable="true"
+          @click="center=m.position"
+      />
+    </GmapMap>
+
+    <Messagerie/>
 
   </div>
 </template>
@@ -29,33 +35,35 @@ import TheNavigation from '@/components/layout/TheNavigation.vue'
 import Messagerie from '@/components/layout/Messagerie.vue'
 
 export default {
-  name: 'About',
+  name: 'Map',
   components: {
-    TheNavigation, 
+    TheNavigation,
     Messagerie
-  }, 
-  mounted () {
+  },
+  mounted() {
     // At this point, the child GmapMap has been mounted, but
     // its map has not been initialized.
     // Therefore we need to write mapRef.$mapPromise.then(() => ...)
- 
+
     this.$refs.mapRef.$mapPromise.then((map) => {
-      map.panTo({lat: 47.495602, lng: 6.805290})
+      map.panTo({lat: 47.495602, lng: 4.805290})
     })
-  }, 
-  data(){
-      return {
-          center: {lat:47.837299, lng:2.353695},
-      markers: [ 
-          {position: { lat: 48.837299, lng: 2.353695}, },
-          {position: { lat: 47.495602, lng: 6.805290}  },
-          {position: { lat: 45.739503, lng: 4.817517}  },
-          {position: { lat: 47.223391, lng: -1.544413} },
-          {position: { lat: 48.682978, lng:  6.161145} },
-          {position: { lat: 47.357118, lng:  7.100343} }
-          
+  },
+  beforeCreate: function () {
+    document.body.style.overflow = "hidden";
+  },
+  data() {
+    return {
+      markers: [
+        {position: {lat: 48.837299, lng: 2.353695},},
+        {position: {lat: 47.495602, lng: 6.805290}},
+        {position: {lat: 45.739503, lng: 4.817517}},
+        {position: {lat: 47.223391, lng: -1.544413}},
+        {position: {lat: 48.682978, lng: 6.161145}},
+        {position: {lat: 47.357118, lng: 7.100343}}
+
       ]
-      };
+    };
   }
 
 }
@@ -63,7 +71,8 @@ export default {
 
 </script>
 
-<style  scoped>
+<style scoped>
+
 
 </style>
 
