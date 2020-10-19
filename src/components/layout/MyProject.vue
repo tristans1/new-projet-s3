7c14fb63b375ff685c6a4aece68f73d084ee8bcb
Tristan Seclet <tristanseclet@gmail.com>
2020-10-19 10:01:37 +0200

    add : Profil.vue with components

---
+++ src/components/layout/MyProject.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="projet" class="mt-5 bg-secondary col-12 col-md-10 offset-md-1 col-lg-8 offset-lg-2 mt-3 d-flex">
+    <div><img src="../img/projet.png" alt="projet 1"></div>
+    <div class=" ml-5 mr-5"><h3 class=" text-left text-primary">Titre du projet</h3>
+      <p class="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dolorum ea numquam porro qui velit voluptate.
+        Ab aliquam asperiores doloremque impedit in, ipsum necessitatibus non quia quod soluta! Modi, sit.</p>
+      <button class="mt-3 pl-5 pr-5 btn btn-primary">Voir plus</button>
+    </div>
+    <!--<button   id="left"><img src="../svg/arrow-toggle.svg" alt="arrow-left"> </button>
+    <button id="right"><img src="../svg/arrow-toggle.svg" alt="arrow-right"></button>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyProject',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+#projet{
+  padding: 33px;
+}
+img{
+  width: 400px;
+}
+
+
+</style>
--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -3,6 +3,19 @@
     <TheNavigation class="d-none d-sm-block" style="z-index: 1"/>
     <TheNavHome class="d-block d-sm-none" style="z-index: 1"/>
     <MenuMobil class="d-block d-sm-none" style="z-index: 1"/>
+    <MyProfil/>
+    <MyProject />
+    <div style="position: absolute; left: 150px; width: 70px; bottom: 3%">
+      <img class="mb-0" src="@/components/svg/mouse.svg" alt="mouse" style=" width: 70px;">
+      <img class=" arrow mt-0" src="@/components/svg/arrow-toggle.svg" alt="arrow" style="width: 20px"><br/>
+      <img class="arrow mt-0" src="@/components/svg/arrow-toggle.svg" alt="arrow" style="width: 20px">
+
+
+    </div>
+
+    <Space class="d-block d-sm-none" style="z-index: 1"/>
+
+
 
   </div>
 </template>
@@ -12,18 +25,27 @@
 import TheNavHome from '@/components/layout/TheNavHome.vue'
 import TheNavigation from '@/components/layout/TheNavigation'
 import MenuMobil from '@/components/layout/MenuMobil'
+import MyProfil from "@/components/layout/MyProfil";
+import MyProject from "@/components/layout/MyProject";
+import Space from "@/components/layout/Space";
 
 export default {
   name: 'Profil',
   components: {
     TheNavHome,
     TheNavigation,
-    MenuMobil
+    MenuMobil,
+    MyProfil,
+    MyProject,
+    Space
   }
 }
 </script>
 
 <style scoped>
+.arrow{
+  transform: rotate(180deg);
+}
 
 </style>
 
